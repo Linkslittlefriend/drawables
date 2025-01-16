b5ae04015787b0c12d35d4dc9fa2e2b049171d20
Linkslittlefriend <mghdamsi@gmail.com>
2025-01-16 22:38:57 +0200

    finish grid functionality, complete extra credit exercise 1

--- drawables.js
+++ drawables.js
@@ -1,22 +1,59 @@
 // draw 16 divs using DOM methods
 
-function drawCanvas(){
-    for(i=0;i<4;i++){
-        console.log(i);
+function generateNewCanvas(mult) {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+
+    drawCanvas(mult);
+}
+
+function randomizeColor(){
+    let r,g,b;
+    r = Math.floor(Math.random() * 255) + 1;
+    g = Math.floor(Math.random() * 255) + 1;
+    b = Math.floor(Math.random() * 255) + 1;
+
+    console.log(`rgb(${r},${g},${b})`);
+
+    return `rgb(${r},${g},${b})`;
+}
+
+function drawCanvas(mult){
+    for(i=0;i<mult;i++){
+        //console.log(i);
         const topdiv = document.createElement("div");
         canvas.appendChild(topdiv);
         topdiv.setAttribute("id","div " + i);
         topdiv.classList.add("topdiv");
-        for(j=0;j<4;j++){
-            console.log("j here: " + j);
+        for(j=0;j<mult;j++){
+            //console.log("j here: " + j);
             const subdiv = document.createElement("div")
             topdiv.appendChild(subdiv);
             subdiv.setAttribute("id","subdiv " + i);
             subdiv.classList.add("subdiv");
+            subdiv.addEventListener("mouseover", () => {
+                console.log("drew on square: " + i + " x " + j);
+                subdiv.style.backgroundColor = randomizeColor();})
         }
     }
 }
 
 const canvas = document.querySelector(".container");
+const drawbtn = document.querySelector("#drawbtn");
 
-drawCanvas();
+drawCanvas(4);
+
+drawbtn.addEventListener("click", () => {
+    let multiplier = +prompt("type in the multipier for squares you wish to generate. (for instance: if you say 4, then the grid will generate 4x4 squares which whill be 16 squares)");
+    let puritytest = true;
+
+    do {
+        console.log(multiplier);
+        if(!puritytest)
+            multiplier = +prompt("incorrect value. Please pick a REAL NON-DECIMAL number between 1 and 100 including those two.");
+        puritytest = typeof(multiplier) === "number" && multiplier <= 100 && multiplier >= 1 && Math.floor(multiplier) === multiplier;
+    } while(!puritytest);
+
+    generateNewCanvas(multiplier);
+})
